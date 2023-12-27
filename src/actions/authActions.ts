b046d0aaa770ca47da74197a5bb744b9352e094d
Peterdinis <peterdinis0@pobox.sk>
2023-12-27 15:57:06 +0100

    feat: New action for register user

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,17 +1,36 @@
-"use server"
+"use server";
 
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-
 export async function loginUser(email: string, password: string) {
-    const supabase = createRouteHandlerClient({cookies});
-    const response = await supabase.auth.signInWithPassword({
-        email,
-        password
-    });
+  const supabase = createRouteHandlerClient({ cookies });
+  const response = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
-    return response;
+  return response;
 }
 
-export async function registerUser(email: string, password: string) {}
+export async function registerUser(email: string, password: string) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { data } = await supabase.from("users").select("*").eq("email", email);
+
+  if (data?.length)
+    return {
+      error: {
+        message: "User already exists",
+      },
+    };
+
+  const respone = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: "http://localhost:3000/api/auth/callback",
+    },
+  });
+
+  return respone;
+}

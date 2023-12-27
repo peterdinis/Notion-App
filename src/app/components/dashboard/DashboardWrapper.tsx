3e4ab94b0c3b81a9fe4ef54cbaae9a4d592d2168
Peterdinis <peterdinis0@pobox.sk>
2023-12-27 19:23:33 +0100

    feat: New emoji picker component

--- src/app/components/dashboard/DashboardWrapper.tsx
+++ src/app/components/dashboard/DashboardWrapper.tsx
@@ -1,58 +1,12 @@
-"use client"
-
-import { FC, useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { FC } from "react";
 import AppHeader from "../shared/AppHeader";
-import { supabaseServerClient } from "@/lib/supabaseSetup";
-import { db } from "@/lib/db";
-import DashboardSetup from "./DashboardSetup";
 
-const DashboardWrapper: FC = () => {
-  const router = useRouter();
-  const [workspace, setWorkspace] = useState<any>(null);
-
-  useEffect(() => {
-    const userResponse = supabaseServerClient.auth.getUser();
-
-    userResponse
-      .then((res) => {
-        const userId = res.data.user?.id;
-        if (userId) {
-          db.workspace
-            .findFirst({
-              where: {
-                userId: userId as unknown as number,
-              },
-            })
-            .then((workspaceData: any) => {
-              setWorkspace(workspaceData);
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        } else {
-          console.log("User ID not found");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  if (!workspace) {
-    return (
-      <div className="bg-background h-screen w-screen flex justify-center items-center">
-        <DashboardSetup  />
-      </div>
-    );
-  } else {
-    router.push(`/dashboard/${workspace.id}`);
-    return (
-      <>
-        <AppHeader />
-      </>
-    );
-  }
-};
+const DashboardWrapper: FC = () =>{
+  return (
+    <main>
+      <AppHeader />
+    </main>
+  )
+}
 
 export default DashboardWrapper;

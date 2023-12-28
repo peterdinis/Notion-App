4c08449e1f1ef86434268a693f3c2a5913cea345
Peterdinis <peterdinis0@pobox.sk>
2023-12-28 10:46:17 +0100

    feat: Create dahboard setup component

--- src/app/components/dashboard/DashboardSetup.tsx
+++ src/app/components/dashboard/DashboardSetup.tsx
@@ -1,39 +1,106 @@
-"use client";
+'use client';
+import { AuthUser } from '@supabase/supabase-js';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { CreateWorkspaceFormSchema } from '@/app/types/WorkspaceTypes';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { Label } from '@radix-ui/react-label';
+import { Loader } from 'lucide-react';
+import { v4 } from 'uuid';
+import {useRouter} from "next/navigation";
+import EmojiPicker from '../shared/EmojiComponent';
+import { supabaseClient } from '@/lib/supabaseSetup';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { FC, useState } from "react";
-import { AuthUser } from "@supabase/supabase-js";
-import EmojiPicker from "../shared/EmojiComponent";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { FieldValues, useForm } from "react-hook-form";
-
-interface IDashboardSetupProps {
+interface DashboardSetupProps {
   user: AuthUser;
 }
 
-const DashboardSetup: FC<IDashboardSetupProps> = ({
+const DashboardSetup: React.FC<DashboardSetupProps> = ({
   user,
-}: IDashboardSetupProps) => {
-  const [selectedEmoji, setSelectedEmoji] = useState("💼");
-  const {register, handleSubmit, reset, formState: {isSubmitting, errors, isLoading}} = useForm<FieldValues>({
-    mode: "onChange",
+}) => {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [selectedEmoji, setSelectedEmoji] = useState('💼');
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting: isLoading, errors },
+  } = useForm<z.infer<typeof CreateWorkspaceFormSchema>>({
+    mode: 'onChange',
     defaultValues: {
-      logo: "",
-      workspace: "",
+      logo: '',
+      workspaceName: '',
     },
   });
+
+  const onSubmit: SubmitHandler<
+    z.infer<typeof CreateWorkspaceFormSchema>
+  > = async (value) => {
+    const file = value.logo?.[0];
+    let filePath = null;
+    const workspaceUUID = v4();
+    console.log(file);
+
+    if (file) {
+      try {
+        const { data, error } = await supabaseClient.storage
+          .from('workspace-logos')
+          .upload(`workspaceLogo.${workspaceUUID}`, file, {
+            cacheControl: '3600',
+            upsert: true,
+          });
+        if (error) throw new Error('');
+        filePath = data.path;
+      } catch (error) {
+        console.log('Error', error);
+        toast({
+          variant: 'destructive',
+          title: 'Error! Could not upload your workspace logo',
+        });
+      }
+    }
+    try {
+      const newWorkspace = {
+        data: null,
+        createdAt: new Date().toISOString(),
+        iconId: selectedEmoji,
+        id: workspaceUUID,
+        inTrash: '',
+        title: value.workspaceName,
+        workspaceOwner: user.id,
+        logo: filePath || null,
+        bannerUrl: '',
+      };
+      toast({
+        title: 'Workspace Created',
+        description: `${newWorkspace.title} has been created successfully.`,
+      });
+
+      router.replace(`/dashboard/${newWorkspace.id}`);
+    } catch (error) {
+      console.log(error, 'Error');
+      toast({
+        variant: 'destructive',
+        title: 'Could not create your workspace',
+        description:
+          "Oops! Something went wrong, and we couldn't create your workspace. Try again or come back later.",
+      });
+    } finally {
+      reset();
+    }
+  };
+
   return (
     <Card
       className="w-[800px]
       h-screen
-      sm:h-auto"
+      sm:h-auto
+  "
     >
       <CardHeader>
         <CardTitle>Create A Workspace</CardTitle>
@@ -41,65 +108,76 @@ const DashboardSetup: FC<IDashboardSetupProps> = ({
           Lets create a private workspace to get you started.You can add
           collaborators later from the workspace settings tab.
         </CardDescription>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-4">
-              <div
-                className="flex
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-4">
+            <div
+              className="flex
             items-center
             gap-4"
-              >
-                <div className="text-5xl">
-                  <EmojiPicker getValue={(emoji) => setSelectedEmoji(emoji)}>
-                    {selectedEmoji}
-                  </EmojiPicker>
-                </div>
-                <div className="w-full">
-                  <Label
-                    htmlFor="workspaceName"
-                    className="text-sm text-muted-foreground"
-                  >
-                    <Input
-                      id="workspaceName"
-                      type="text"
-                      placeholder="Worksapce Name"
-                      className="bg-transparent"
-                      disabled={isLoading}
-                      {...register("workspaceName", {
-                        required: "Workspace is requeired"
-                      })}
-                    />
-                    <small className="text-red-700">
-                      {errors?.workspaceName?.message?.toString()}
-                    </small>
-                  </Label>
-                </div>
+            >
+              <div className="text-5xl">
+                <EmojiPicker getValue={(emoji: string) => setSelectedEmoji(emoji)}>
+                  {selectedEmoji}
+                </EmojiPicker>
               </div>
-              <div className="w-full">
-                  <Label
-                    htmlFor="workspaceLogo"
-                    className="text-sm text-muted-foreground"
-                  >
-                    <Input
-                      id="workspaceName"
-                      type="file"
-                      accept="image/*"
-                      placeholder="Worksapce Logo"
-                      className="bg-transparent"
-                      disabled={isLoading}
-                      {...register("workspaceName", {
-                        required: "Workspace is requeired"
-                      })}
-                    />
-                    <small className="text-red-700">
-                      {errors?.workspaceLogo?.message?.toString()}
-                    </small>
-                  </Label>
-                </div>
+              <div className="w-full ">
+                <Label
+                  htmlFor="workspaceName"
+                  className="text-sm
+                  text-muted-foreground
+                "
+                >
+                  Name
+                </Label>
+                <Input
+                  id="workspaceName"
+                  type="text"
+                  placeholder="Workspace Name"
+                  disabled={isLoading}
+                  {...register('workspaceName', {
+                    required: 'Workspace name is required',
+                  })}
+                />
+                <small className="text-red-600">
+                  {errors?.workspaceName?.message?.toString()}
+                </small>
+              </div>
             </div>
-          </form>
-        </CardContent>
-      </CardHeader>
+            <div>
+              <Label
+                htmlFor="logo"
+                className="text-sm
+                  text-muted-foreground
+                "
+              >
+                Workspace Logo
+              </Label>
+              <Input
+                id="logo"
+                type="file"
+                accept="image/*"
+                placeholder="Workspace Name"
+                {...register('logo', {
+                  required: false,
+                })}
+              />
+              <small className="text-red-600">
+                {errors?.logo?.message?.toString()}
+              </small>
+            </div>
+            <div className="self-end">
+              <Button
+                disabled={isLoading}
+                type="submit"
+              >
+                {!isLoading ? 'Create Workspace' : <Loader />}
+              </Button>
+            </div>
+          </div>
+        </form>
+      </CardContent>
     </Card>
   );
 };
--- src/app/types/WorkspaceTypes.ts
+++ src/app/types/WorkspaceTypes.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const CreateWorkspaceFormSchema = z.object({
     workspaceName: z.string().describe("Workspace Name").min(1, "Workspace must have one string"),
-    workspaceFile: z.any()
+    logo: z.any()
 })

e378b891036bedfdcc7acd8918f097d8f954aa5b
Peterdinis <peterdinis0@pobox.sk>
2023-12-28 13:23:07 +0100

    feat: Start working on sidebar

--- src/app/components/shared/Sidebar.tsx
+++ src/app/components/shared/Sidebar.tsx
@@ -1,11 +1,24 @@
+import { supabaseServerClient } from "@/lib/supabaseSetup";
+import { cn } from "@/lib/utils";
 import { FC } from "react";
 
 interface ISidebarProps {
   params: any;
+  className?: string
 }
 
-const Sidebar: FC<ISidebarProps> = ({params}: ISidebarProps) => {
-  return <></>;
+const Sidebar: FC<ISidebarProps> = async ({params, className}: ISidebarProps) => {
+  const supabase = supabaseServerClient;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  return <aside className={cn("hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between", className)}>
+    
+  </aside>;
 };
 
 export default Sidebar;

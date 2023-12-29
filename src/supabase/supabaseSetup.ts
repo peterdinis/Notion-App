7c4375779b0b502b2a49982b683ed910b02778f0
Peterdinis <peterdinis0@pobox.sk>
2023-12-29 14:21:47 +0100

    feat: New provider to layout

--- src/supabase/supabaseSetup.ts
+++ src/supabase/supabaseSetup.ts
@@ -1,19 +1,7 @@
-import {
-    createRouteHandlerClient,
-    createServerComponentClient,
-} from '@supabase/auth-helpers-nextjs';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 export const supabaseClient = createRouteHandlerClient(
-    { cookies },
-    {
-        supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL as unknown as string,
-        supabaseKey: process.env
-            .NEXT_PUBLIC_SUPABASE_AVON_KEY as unknown as string,
-    },
-);
-
-export const supabaseServerClient = createServerComponentClient(
     { cookies },
     {
         supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL as unknown as string,

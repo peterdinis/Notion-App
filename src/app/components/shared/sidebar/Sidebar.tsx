27d43e34c0b4ad7129b350d3b9f2acc42e645cb4
Peterdinis <peterdinis0@pobox.sk>
2023-12-30 16:30:44 +0100

    FOO

--- src/app/components/shared/sidebar/Sidebar.tsx
+++ src/app/components/shared/sidebar/Sidebar.tsx
@@ -1,56 +1,102 @@
-import { cn } from '@/lib/utils';
-import { FC } from 'react';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { getFolders } from '@/supabase/queries/queries';
-import WorkspaceDropdown from '../../workspace/WorkspaceDropdown';
+import React from 'react';
 
-interface ISidebarProps {
-    params: any;
-    className?: string;
+import { cookies } from 'next/headers';
+import {
+  getCollaboratingWorkspaces,
+  getFolders,
+  getPrivateWorkspaces,
+  getSharedWorkspaces,
+  getUserSubscriptionStatus,
+} from '@/lib/supabase/queries';
+import { redirect } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+import WorkspaceDropdown from './workspace-dropdown';
+import NativeNavigation from './native-navigation';
+import { ScrollArea } from '../ui/scroll-area';
+import FoldersDropdownList from './folders-dropdown-list';
+
+interface SidebarProps {
+  params: { workspaceId: string };
+  className?: string;
 }
 
-const Sidebar: FC<ISidebarProps> = async ({
-    className,
-    params,
-}: ISidebarProps) => {
-    const supabase = createServerComponentClient(
-        { cookies },
-        {
-            supabaseUrl: process.env
-                .NEXT_PUBLIC_SUPABASE_URL as unknown as string,
-            supabaseKey: process.env
-                .NEXT_PUBLIC_SUPABASE_AVON_KEY as unknown as string,
-        },
-    );
+const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
+  const supabase = createServerComponentClient({ cookies });
+  //user
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
+  if (!user) return;
 
-    if (!user) return;
+  //subscr
+  const { data: subscriptionData, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
 
-    const { data, error } = await getFolders(params.workspaceId); // string vs int bug must be fixed
+  //folders
+  const { data: workspaceFolderData, error: foldersError } = await getFolders(
+    params.workspaceId
+  );
+  //error
+  if (subscriptionError || foldersError) redirect('/dashboard');
 
-    console.log(data, error);
+  const [privateWorkspaces, collaboratingWorkspaces, sharedWorkspaces] =
+    await Promise.all([
+      getPrivateWorkspaces(user.id),
+      getCollaboratingWorkspaces(user.id),
+      getSharedWorkspaces(user.id),
+    ]);
 
-    return (
-        <aside
-            className={cn(
-                'hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between',
-                className,
-            )}
+  //get all the different workspaces private collaborating shared
+  return (
+    <aside
+      className={twMerge(
+        'hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between',
+        className
+      )}
+    >
+      <div>
+        <WorkspaceDropdown
+          privateWorkspaces={privateWorkspaces}
+          sharedWorkspaces={sharedWorkspaces}
+          collaboratingWorkspaces={collaboratingWorkspaces}
+          defaultValue={[
+            ...privateWorkspaces,
+            ...collaboratingWorkspaces,
+            ...sharedWorkspaces,
+          ].find((workspace) => workspace.id === params.workspaceId)}
+        />
+        <PlanUsage
+          foldersLength={workspaceFolderData?.length || 0}
+          subscription={subscriptionData}
+        />
+        <NativeNavigation myWorkspaceId={params.workspaceId} />
+        <ScrollArea
+          className="overflow-scroll relative
+          h-[450px]
+        "
         >
-            <div>
-                <WorkspaceDropdown
-                    privateWorkspaces={[]}
-                    sharedWorkspaces={[]}
-                    collaboratingWorkspaces={[]}
-                    defaultValue={undefined}
-                />
-            </div>
-        </aside>
-    );
+          <div
+            className="pointer-events-none 
+          w-full 
+          absolute 
+          bottom-0 
+          h-20 
+          bg-gradient-to-t 
+          from-background 
+          to-transparent 
+          z-40"
+          />
+          <FoldersDropdownList
+            workspaceFolders={workspaceFolderData || []}
+            workspaceId={params.workspaceId}
+          />
+        </ScrollArea>
+      </div>
+      <UserCard subscription={subscriptionData} />
+    </aside>
+  );
 };
 
 export default Sidebar;

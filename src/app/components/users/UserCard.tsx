63574cee00102fefee9423dd90d54d2097508511
Peterdinis <peterdinis0@pobox.sk>
2023-12-30 16:10:18 +0100

    FOO

--- src/app/components/users/UserCard.tsx
+++ src/app/components/users/UserCard.tsx
@@ -2,12 +2,29 @@ import { FC } from 'react';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { db } from '@/lib/db';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { LogOut } from 'lucide-react';
 import LogoutBtn from '../shared/LogoutBtn';
+import ModeToggle from '../shared/ModeToggle';
 
-const UserCard: FC = () => {
-    return <></>;
+const UserCard: FC = async () => {
+    const supabase = createServerComponentClient({ cookies });
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if(!user) return;
+
+    const response = await db.user.findFirst({
+        where: {
+            id: user?.id
+        }
+    })
+
+    return (
+        <>
+        
+        </>
+    )
 };
 
 export default UserCard;

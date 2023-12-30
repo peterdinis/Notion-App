56cc5a92114fdbe62405a3b7c1401099a36dccd8
Peterdinis <peterdinis0@pobox.sk>
2023-12-30 16:08:36 +0100

    feat: Working on sidebar

--- src/app/components/users/UserCard.tsx
+++ src/app/components/users/UserCard.tsx
@@ -1,11 +1,13 @@
-import { FC } from "react";
+import { FC } from 'react';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { db } from '@/lib/db';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { LogOut } from 'lucide-react';
+import LogoutBtn from '../shared/LogoutBtn';
 
 const UserCard: FC = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+    return <></>;
+};
 
 export default UserCard;

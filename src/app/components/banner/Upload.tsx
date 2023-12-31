1d0f4f603a20710af16517bb23927f498e5d223b
Peterdinis <peterdinis0@pobox.sk>
2023-12-31 09:46:29 +0100

    FOO

--- src/app/components/banner/Upload.tsx
+++ src/app/components/banner/Upload.tsx
@@ -1,35 +1,30 @@
+'use client';
+import { FC, ReactNode } from 'react';
+import BannerUploadForm from './UploadForm';
+import CustomDialogTrigger from '../shared/CustomDialog';
 
-  import React from 'react';
-  import CustomDialogTrigger from '../global/custom-dialog-trigger';
-  import BannerUploadForm from './banner-upload-form';
-  
-  interface BannerUploadProps {
-    children: React.ReactNode;
+interface BannerUploadProps {
+    children: ReactNode;
     className?: string;
     dirType: 'workspace' | 'file' | 'folder';
     id: string;
-  }
-  
-  const BannerUpload: React.FC<BannerUploadProps> = ({
+}
+
+const BannerUpload: FC<BannerUploadProps> = ({
     id,
     dirType,
     children,
     className,
-  }) => {
+}) => {
     return (
-      <CustomDialogTrigger
-        header="Upload Banner"
-        content={
-          <BannerUploadForm
-            dirType={dirType}
-            id={id}
-          />
-        }
-        className={className}
-      >
-        {children}
-      </CustomDialogTrigger>
+        <CustomDialogTrigger
+            header='Upload Banner'
+            content={<BannerUploadForm dirType={dirType} id={id} />}
+            className={className}
+        >
+            {children}
+        </CustomDialogTrigger>
     );
-  };
-  
-  export default BannerUpload;
+};
+
+export default BannerUpload;

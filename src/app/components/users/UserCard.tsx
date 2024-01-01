0a70bfc21ccdb498949eb5aaeae4cf60b95a1052
Peterdinis <peterdinis0@pobox.sk>
2024-01-01 18:18:29 +0100

    fix: Fixing some bugs WIP

--- src/app/components/users/UserCard.tsx
+++ src/app/components/users/UserCard.tsx
@@ -1,18 +1,12 @@
 import { FC } from 'react';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
 import { db } from '@/lib/db';
 import { LogOut } from 'lucide-react';
 import LogoutBtn from '../shared/LogoutBtn';
 import ModeToggle from '../shared/ModeToggle';
+import { useSupabaseUser } from '@/supabase/providers/UserProvider';
 
 const UserCard: FC = async () => {
-    const supabase = createServerComponentClient({ cookies });
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-
-    if (!user) return;
+    const {user} = useSupabaseUser();
 
     const response = await db.user.findFirst({
         where: {
